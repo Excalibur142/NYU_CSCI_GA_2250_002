a5549c56b9413b6461a84488931be151fd7b68e9
Excalibur142 <62149678+Excalibur142@users.noreply.github.com>
2021-10-06 01:27:19 -0400

    Added Substantial Changes
    Added Command parsers that takes stdin and parses the string by empty space tokens. Also began building architecture for reading and understanding commands.

--- NYUSH/nyush.c
+++ NYUSH/nyush.c
@@ -13,6 +13,8 @@ void close_shell(char* CloseVal) {
 }
 //Gets length of array
 int get_length(char* ToCount) {
+	if (ToCount == NULL)
+		return 0;
 	int i = 0;
 	while (ToCount[i] != '\0') {
 		i++;
@@ -21,6 +23,7 @@ int get_length(char* ToCount) {
 }
 //Call when changes to directory are made!
 //Otherwise use global directory value
+
 void make_dir(char **directory) {
 	if (directory == NULL)
 		return;
@@ -50,28 +53,119 @@ void make_dir(char **directory) {
 	}
 	free(DirName);
 }
+//Redirect Functions
+void redirect_output();
+void redirect_input();
+//Pipe function
+void pipe_cmd();
+//Run command and arg
+void run_command(char** ParsedLine, int Args);
+
+//Will go through a list of commands with the structure of command and arg
+//Do something with the given commands
+void examine_command(char** ParsedLine, int Args, char* Prompt) {
+	//Handle 4 simple commands
+	if (!strcmp(ParsedLine[0], "cd\n") || !strcmp(ParsedLine[0], "cd")) {
+		if (Args == 1 || Args > 2) {
+			printf("%sError: Invalid command\n", Prompt);
+			return;
+		}
+	}
+	if (!strcmp(ParsedLine[0], "jobs\n") || !strcmp(ParsedLine[0], "jobs")) {
+		if (Args > 1) {
+			printf("%sError: Invalid command\n", Prompt);
+			return;
+		}
+	}
+	if (!strcmp(ParsedLine[0], "fg\n") || !strcmp(ParsedLine[0], "fg")) {
+		if (Args == 1 || Args > 2) {
+			printf("%sError: Invalid command\n", Prompt);
+			return;
+		}
+	}
+	if (!strcmp(ParsedLine[0], "exit\n") || !strcmp(ParsedLine[0], "exit")) {
+		//Handle suspended jobs
+		if (Args > 1) {
+			printf("%sError: Invalid command\n", Prompt);
+			return;
+		}
+		exit(0);
+	}
+	//Checks to see if there are more than 2 arguments
+	if (Args >= 2) {
+		if (ParsedLine[1][0] == '<') {
+			if (Args < 3) {
+				printf("%sError: No Input File\n", Prompt);
+				return;
+			}
+			
+		}
+	}
+	
+}
+
 //Get and parse command line
-//Each command is terminated by enter key or \n newline
+//Each command is seperated by space
+char** parse_command(char* command, int CountOfSpaces) {
+	//Accounts for last space, or if there is only 1
+	CountOfSpaces++;
+	char** ParsedCommand = malloc((CountOfSpaces) * sizeof(char*));
+	char* SingleCommand = strtok(command, " ");
+	int i = 0;
+	while (SingleCommand != NULL) {
+		ParsedCommand[i] = calloc((get_length(SingleCommand) + 1), sizeof(char*));
+		for (int j = 0; j < get_length(SingleCommand); ++j) {
+			ParsedCommand[i][j] = SingleCommand[j];
+		}
+		++i;
+		SingleCommand = strtok(NULL, " ");
+	}
+	return ParsedCommand;
+}
+void free_command(char** command, int CountOfSpaces) {
+	for (int i = 0; i < CountOfSpaces; ++i)
+		free(command[i]);
+	free(command);
+}
 
 
 int main(int argc, const char* const* argv) {
 	//Create variable to store current directory in
 	//This way only get_dir() needs to be called if directory changes
-	char* ReturnVal = calloc(10, sizeof(char*));
-	if (ReturnVal == NULL) {
+	char* CmdPrompt = calloc(10, sizeof(char*));
+	if (CmdPrompt == NULL) {
 		perror("calloc failed");
 		exit(1);
 	}
-	make_dir(&ReturnVal);
-	if(ReturnVal !=NULL)
-		printf("%s\n",ReturnVal);
-	
-
+	make_dir(&CmdPrompt);
+	char Command[1001];
 	//Create a while loop that waits for exit and exits on command
-	//while (1) {
+	while (1) {
+		if (CmdPrompt != NULL)
+			printf("%s", CmdPrompt);
+		fgets(Command, 1001, stdin);
+		//Check command length and prompt for new one
+		if (strlen(Command) > 999) {
+			printf("%sError: Your command was over 1000 characters\n",CmdPrompt);
+			fflush(stdin);
+			continue;
+		}
+		//Get count of arguments
+		int CountOfArgs = 0;
+		for (int i = 0; i < get_length(Command); ++i) {
+			if (Command[i] == ' ')
+				CountOfArgs++;
+		}
+		//Accounts for last space, or if there is only 1
+		CountOfArgs++;
+
+		char** cmdline = parse_command(Command, CountOfArgs);
+		examine_command(cmdline, CountOfArgs, CmdPrompt);
 
 
-	//}
+		//Free the cmdline
+		free_command(cmdline, CountOfArgs);
+	}
 
-	close_shell(ReturnVal);
+	close_shell(CmdPrompt);
 }
