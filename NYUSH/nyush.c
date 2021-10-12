c12dbad02919ad103665995afc7fcd068fe3500d
Asad Ranavaya <62149678+Excalibur142@users.noreply.github.com>
2021-10-12 17:27:01 -0400

    Major Revisions
    Added signal handling, multiple piping, appending text, and general output and input redirect works with n pipes.

--- NYUSH/nyush.c
+++ NYUSH/nyush.c
@@ -8,10 +8,18 @@
 #include <sys/wait.h>
 #include <stdbool.h>
 #include <sys/stat.h>
+#include <signal.h>
 
 //make variadic to close as many pointers as possible
 void close_shell(char* CloseVal) {
 	free(CloseVal);
+	printf("\n");
+}
+
+void free_command(char** command, int CountOfSpaces) {
+	for (int i = 0; i < CountOfSpaces; ++i)
+		free(command[i]);
+	free(command);
 }
 //Gets length of array
 int get_length(char* ToCount) {
@@ -32,7 +40,7 @@ void make_dir(char **directory) {
 	char* DirName = get_current_dir_name();
 	//Check if DirName is empty, if so return error
 	if (DirName == NULL) {
-		perror("get_current_dir_name");
+		fprintf(stderr, "Error: invalid command\n");
 		return;
 	}
 	int DirLength = get_length(DirName);
@@ -55,201 +63,172 @@ void make_dir(char **directory) {
 	}
 	free(DirName);
 }
-//Redirect Functions
-void redirect_output();
-void redirect_input();
-//Pipe function
-void pipe_cmd();
-//Run command and arg
-void run_command(char** ParsedLine, int Args);
 
-char invalid_arg[] = {'>','<','|','*','!', '`', '\'','\"'};
+//Redirect Functions
+int redirect_output(char* FileToRedirect, _Bool ShouldAppend) {
+	int fd1;
+	if (ShouldAppend) {
+		fd1 = open(FileToRedirect, O_WRONLY | O_APPEND, 0600);
+	}
+	else {
+		fd1 = open(FileToRedirect, O_WRONLY | O_TRUNC | O_CREAT, 0600);
+	}
+	//If file cant be opened or created, throw
+	if (fd1 == -1) {
+		fprintf(stderr, "Error: invalid command\n");
+		return -1;
+	}
+	if (dup2(fd1, 1) == -1) {
+		fprintf(stderr, "Error: invalid command\n");
+		return -1;
+	}
+	if (close(fd1) == -1) {
+		fprintf(stderr, "Error: invalid command\n");
+		return-1;
+	}
+	return 0;
+}
+int redirect_input(char* FileToRedirect) {
+	int fd0 = open(FileToRedirect, O_RDONLY);
+	//If file doesnt exist or cant be opened, throw
+	if (fd0 == -1) {
+		fprintf(stderr, "Error: invalid command\n");
+		return -1;
+	}
+	if(dup2(fd0, 0) == -1) {
+		fprintf(stderr, "Error: invalid command\n");
+		return -1;
+	}
+	if (close(fd0) == -1) {
+		fprintf(stderr, "Error: invalid command\n");
+		return -1;
+	}
+	return 0;
+}
+
 
 //Will go through a list of commands with the structure of command and arg
-//Do something with the given commands
-void examine_command(char** ParsedLine, int Args, char* Prompt) {
+void examine_command(char** ParsedLine, int Args, char* Prompt, _Bool* Cont) {
 	//Handle 4 simple commands
 	if (!strcmp(ParsedLine[0], "cd\n") || !strcmp(ParsedLine[0], "cd")) {
+		*Cont = true;
 		if (Args == 1 || Args > 2) {
-			printf("%sError: Invalid command\n", Prompt);
+			fprintf(stderr, "Error: invalid command\n");
 			return;
 		}
 		if (chdir(ParsedLine[1]) == -1) {
-			printf("%sError: Invalid directory\n", Prompt);
+			fprintf(stderr, "Error: invalid command\n");
 			return;
 		}
 		return;
 	}
 	if (!strcmp(ParsedLine[0], "jobs\n") || !strcmp(ParsedLine[0], "jobs")) {
+		*Cont = true;
 		if (Args > 1) {
-			printf("%sError: Invalid command\n", Prompt);
+			fprintf(stderr, "Error: invalid command\n");
 			return;
 		}
 	}
 	if (!strcmp(ParsedLine[0], "fg\n") || !strcmp(ParsedLine[0], "fg")) {
+		*Cont = true;
 		if (Args == 1 || Args > 2) {
-			printf("%sError: Invalid command\n", Prompt);
+			fprintf(stderr, "Error: invalid command\n");
 			return;
 		}
 	}
 	if (!strcmp(ParsedLine[0], "exit\n") || !strcmp(ParsedLine[0], "exit")) {
 		//Handle suspended jobs
+		*Cont = true;
 		if (Args > 1) {
-			printf("%sError: Invalid command\n", Prompt);
+			fprintf(stderr, "Error: invalid command\n");
 			return;
 		}
+		free_command(ParsedLine, Args);
+		close_shell(Prompt);
 		exit(0);
 	}
+}
+char invalid_arg[] = {'>','<','|','*','!', '`', '\'','\"'};
 
-	//Check the command and see if there are any <, or |, or <, or <<. If none, execute the command and all arguments
-	//Create a flag every time a pipe has been seen and the previous pipe to create a parsed char** of cmd arg between pipes
-	int Flag = 0;
-	_Bool ChangeFlag = true;
-	for (int i = 0; i < Args; ++i) {
-		
-		//Checks for < > or |
-		if (strstr(ParsedLine[i], "<") || strstr(ParsedLine[i], "|")) {
-			//Set flag as new starting point for handling pipe and <
-			//Handle redirecting input
-			if (strstr(ParsedLine[i], "<")) { //****************************************************************** FIX MULTIPLE INPUT REDIRECTS, CURRENTLY DOES ALL? PLUS COMMANDS IN BETWEEN?
-				ParsedLine[i] = NULL;
-				int pid = fork();
-				if (pid == 0) {
-					int fd0 = open(ParsedLine[i + 1], O_RDONLY);
-					if (fd0 == -1) {
-						printf("%sError: Cant open file in input direct\n", Prompt);
-						exit(-1);
-					}
-					fchmod(fd0, 0600);
-					dup2(fd0, STDIN_FILENO);
-					close(fd0);
-					if (execvp(ParsedLine[0], ParsedLine) == -1) {
-						printf("%sError: Invalid command\n", Prompt);
-						exit(-1);
-					}
-				}
-				else {
-					wait(NULL);
-				}
-			
-			}
-			//Handle Piping
-			else if (strstr(ParsedLine[i], "|")) { //************************************************FIX TO ONLY RUN 2ND PIPE UP TO 2ND COMMAND
-				ParsedLine[i] = NULL;
-				int pipefd[2];
-				int status =0;
-				pid_t p1, p2;
-				if (pipe(pipefd) == -1) {
-					printf("%sPipe Failed", Prompt);
-					return;
-				}
-				p1 = fork();
-				if (p1 == 0) {
-					dup2(pipefd[1], 1);
-					close(pipefd[0]);
-					execvp(ParsedLine[Flag], ParsedLine+Flag);
-					exit(-1);
-				}
-				p2 = fork();
-				if (p2 == 0) {
-					dup2(pipefd[0], 0);
-					close(pipefd[1]);
-					execvp(ParsedLine[i + 1], ParsedLine + i + 1);
-					exit(-1);
-				}
-				else {
-					wait(NULL);
-				}
-							
-			}
-			
-			if (ChangeFlag == true) {
-				Flag = i+1;
-				ChangeFlag = false;
-			}
-			else
-				ChangeFlag = false;
-			//Continue because i at this point is null -> go to next value to check
-			continue;
-		}
-
-		//Handle output redirect
-		if (strstr(ParsedLine[i], ">")) {
-			ParsedLine[i] = NULL;
-			int pid = fork();
-			if (pid == 0) {
-				//In child
-				int fd0;
-				if ((fd0 = open(ParsedLine[i + 1], O_WRONLY | O_CREAT | O_TRUNC)) == -1) {
-					printf("%sError: Cant open file in redirect\n", Prompt);
-					exit(-1);
-				}
-				fchmod(fd0, 0600);
-				dup2(fd0, STDOUT_FILENO);
-				close(fd0);
-				if (execvp(ParsedLine[0], ParsedLine) == -1) {
-					printf("%sError: Invalid command\n", Prompt);
-					exit(-1);
-				}
-			}
-			else {
-				//parent waits for redirect to complete
-				wait(NULL);
-			}
-
-			return;
-		}
-	}
-
-	if (Flag > 0)
-		return;
-
-
+//Run command and arg
+//Do something with the given commands
+void run_command(char** ParsedLine, char* Prompt, int Flag){
 	//Run a simple command
-	//Check if command or argument contain any invalid arguments
-	for (int i = 0; i < Args; ++i) {
-		for (int j = 0; j < 8; ++j) {
-			if (*ParsedLine[i] == invalid_arg[j]) {
-				printf("%sError: Invalid command\n", Prompt);
-				return;
-			}
-		}
-	}
+	////Check if command or argument contain any invalid arguments
+	//for (int i = 0; i < Args; ++i) {
+	//	for (int j = 0; j < 8; ++j) {
+	//		if (*ParsedLine[i] == invalid_arg[j]) {
+	//			printf("%sError: Invalid command\n", Prompt);
+	//			return;
+	//		}
+	//	}
+	//}
 	//Otherwise execute command
 	int ChildID = fork();
 	if(ChildID == 0) {
 		//First Check Absolute path
-		if (ParsedLine[0][0] == '/') {
-			if(execv(ParsedLine[0], ParsedLine) == -1)
-				printf("%sError: Invalid Program or relative path\n", Prompt);
+		if (ParsedLine[Flag][0] == '/') {
+			if(execv(ParsedLine[Flag], ParsedLine+Flag) == -1)
+				fprintf(stderr, "Error: invalid command\n");
 			exit(0);
 		}
 		//Then check Relative path
 		if (ParsedLine[0][0] == '.' && ParsedLine[0][1] == '/') {
 			char PathRel[] = "./";
-			strcat(PathRel, ParsedLine[0]);
-			if (execv(PathRel, ParsedLine) == -1)
-				printf("%sError: Invalid Program\n", Prompt);
+			strcat(PathRel, ParsedLine[Flag]);
+			if (execv(PathRel, ParsedLine+Flag) == -1)
+				fprintf(stderr, "Error: invalid command\n");
 			exit(0);
 		}
 		//Then check /bin and usr/bin
 		char PathBin[] ="/bin/";
-		strcat(PathBin, ParsedLine[0]);
-		if (execv(PathBin, ParsedLine) == -1) {
+		strcat(PathBin, ParsedLine[Flag]);
+		if (execv(PathBin, ParsedLine+Flag) == -1) {
 			char PathUsr[] = "/usr/bin/";
-			strcat(PathUsr, ParsedLine[0]);
-			if (execv(PathUsr, ParsedLine) == -1) 
-				printf("%sError: Invalid Program\n", Prompt);
+			strcat(PathUsr, ParsedLine[Flag]);
+			if (execv(PathUsr, ParsedLine+Flag) == -1) 
+				fprintf(stderr, "Error: invalid command\n");
 		}
 		exit(0);
 	}
-	wait(NULL);
+	else {
+		waitpid(ChildID, NULL, 0);
+	}
+	redirect_input("/dev/tty");
+	redirect_output("/dev/tty", false);
 }
+//Pipe function
+void pipe_cmd(char** ParsedLine, char* Prompt, int Flag) {
+	int pipe_fd[2];
+	if (pipe(pipe_fd) == -1) {
+		fprintf(stderr, "Error: Cant create pip\n");
+		return;
+	}
+	if (dup2(pipe_fd[1], 1) == -1) {
+		fprintf(stderr, "Error: cant dup\n");
+		return;
+	}
+	if (close(pipe_fd[1]) == -1) {
+		fprintf(stderr, "Error: Cant close pipe\n");
+		return;
+	}
+	//Run the first command, the next command will either get pipe'd or ran as the final command
+	run_command(ParsedLine, Prompt, Flag);
+	if (dup2(pipe_fd[0], 0) == -1) {
+		fprintf(stderr, "Error: cant dup\n");
+		return;
+	}
+	if (close(pipe_fd[0]) == -1) {
+		fprintf(stderr, "Error: cant close pipe\n");
+		return;
+	}
+}
+
 
 //Get and parse command line
 //Each command is seperated by space
-char** parse_command(char* command, int CountOfSpaces) {
-	char** ParsedCommand = malloc((CountOfSpaces+1) * sizeof(char*));
+char** parse_command(char* command, int* CountOfSpaces) {
+	char** ParsedCommand = malloc((*CountOfSpaces+1) * sizeof(char*));
 	char* SingleCommand = strtok(command, " ");
 	int i = 0;
 	while (SingleCommand != NULL) {
@@ -260,38 +239,48 @@ char** parse_command(char* command, int CountOfSpaces) {
 		++i;
 		SingleCommand = strtok(NULL, " ");
 	}
-	ParsedCommand[CountOfSpaces] = '\0';
-	if (CountOfSpaces == 1) {
+	*CountOfSpaces = i;
+	ParsedCommand[*CountOfSpaces] = '\0';
+	if (*CountOfSpaces == 1) {
 		ParsedCommand[0][get_length(ParsedCommand[0])-1] = '\0';
 	}else
-		ParsedCommand[CountOfSpaces-1][get_length(ParsedCommand[CountOfSpaces-1])-1] = '\0';
+		ParsedCommand[*CountOfSpaces-1][get_length(ParsedCommand[*CountOfSpaces-1])-1] = '\0';
 
 	return ParsedCommand;
 }
-void free_command(char** command, int CountOfSpaces) {
-	for (int i = 0; i < CountOfSpaces; ++i)
-		free(command[i]);
-	free(command);
+//Signal Handling
+void sig_handler(int temp) {
+	
 }
 
-
 int main(int argc, const char* const* argv) {
+	signal(SIGINT, sig_handler);
+	signal(SIGQUIT, sig_handler);
+	signal(SIGTERM, sig_handler);
+	signal(SIGTSTP, sig_handler);
 	//Create variable to store current directory in
 	//This way only get_dir() needs to be called if directory changes
 	char* CmdPrompt = calloc(10, sizeof(char*));
 	if (CmdPrompt == NULL) {
-		perror("calloc failed");
+		fprintf(stderr, "Error: calloc failed\n");
 		exit(1);
 	}
 	char Command[1001];
 	//Create a while loop that waits for exit and exits on command
+
 	while (1) {
 		make_dir(&CmdPrompt);
 		if (CmdPrompt != NULL)
 			printf("%s", CmdPrompt);
-		fgets(Command, 1001, stdin);
+		if (fgets(Command, 1000, stdin) == NULL) {
+			exit(0);
+		}
+		if (Command[0] == '\n') {
+			//Ignore and prompt again
+			continue;
+		}
 		//Check command length and prompt for new one
-		if (strlen(Command) > 999) {
+		if (strlen(Command) > 1000) {
 			printf("%sError: Your command was over 1000 characters\n",CmdPrompt);
 			fflush(stdin);
 			continue;
@@ -305,13 +294,52 @@ int main(int argc, const char* const* argv) {
 		//Accounts for last space, or if there is only 1
 		CountOfArgs++;
 
-		char** cmdline = parse_command(Command, CountOfArgs);
-		examine_command(cmdline, CountOfArgs, CmdPrompt);
-		
+		_Bool ShouldContinue = false;
+		char** cmdline = parse_command(Command, &CountOfArgs);
+		examine_command(cmdline, CountOfArgs, CmdPrompt, &ShouldContinue);
+		//Go through entered command and check for pipes and redirects
+		int Flag = 0;
+		_Bool ChangeFlag = true;
+		for (int i = 0; i < CountOfArgs; ++i) {
+			if (strstr(cmdline[i], "<")) {
+				if (redirect_input(cmdline[i + 1]) == -1) {
+					ShouldContinue = true;
+					break;
+				}
+				cmdline[i] = NULL;
+				pipe_cmd(cmdline, CmdPrompt, Flag);
+			}
+			else if (strstr(cmdline[i], ">")) {
+				if (strstr(cmdline[i], ">>")) {
+					if (redirect_output(cmdline[i + 1], true) == -1) {
+						ShouldContinue = true;
+						break;
+					}
+				}
+				else if (redirect_output(cmdline[i + 1], false) == -1) {
+					ShouldContinue = true;
+					break;
+				}
+				cmdline[i] = NULL;
+			}
+			else if (strstr(cmdline[i], "|")) {
+				//Found pipe, set flag 1 to location after operation so next command executes after pipe
+				cmdline[i] = NULL;
+				pipe_cmd(cmdline, CmdPrompt,Flag);
+				Flag = i + 1;
+			}
+		}
+		if (ShouldContinue == true) {
+			ShouldContinue = false;
+			continue;
+		}
+		run_command(cmdline, CmdPrompt,Flag);
+
 
 		//Free the cmdline
 		free_command(cmdline, CountOfArgs);
 	}
 
+	//Should never get here because exit will take care of it
 	close_shell(CmdPrompt);
 }
