e5422f571d3c1287368d9b9302bec368dc61d9b3
Excalibur142 <62149678+Excalibur142@users.noreply.github.com>
2021-11-03 12:16:12 -0400

    Fixed Sequential Encoder
    Encodes files sequentially using RLE. Reads chunks of 4kb and stores them in 8kb char arrays (in case there are no consecutive letters) and then stitches them together in a 2gb heap array. Program prints to standard out the concatenated form of the 2gb array combining like chunk ending and beginnings.

--- NYUENC/nyuenc.c
+++ NYUENC/nyuenc.c
@@ -7,28 +7,40 @@
 #include <fcntl.h>
 #include <sys/stat.h>
 
+#define ARR_SIZE 2147483648
+
 //Use getopt to see if -j jobs were added, if so read argv of files from after jobs, if not just read argv of jobs
 
-void encode_File(off_t FileSize, char* file) {
+void encode_File(off_t FileSize, char* file, int* offset, char *arr) {
 	unsigned char count;
 	char letter;
-	for (int i = 0; i < FileSize; ++i) {
+	int j = 0;
+	for (int i = *offset; i < FileSize+*offset; ++i) {
+		if (!file[i])
+			break;
 		letter = file[i];
 		count++;
 		while (file[i] == file[i + 1]) {
 			count++;
 			i++;
 		}
-		printf("%c%c", file[i], count);
+		arr[j] = file[i];
+		arr[j + 1] = count;
+		j+=2;
+		//printf("%c%c", file[i], count);
 		count = 0;
 	}
 	return;
 }
 
-int main(int argc, char* const *argv) {
+int main(int argc, char* const* argv) {
 	//read through argv
 	int optIndex;
 	int threadCount;
+	int chunkSize = 4000;
+	int fileOffset = 0;
+	//int arrOffset = 0;
+	char* encode = malloc(ARR_SIZE);
 	//Get amount of threads to create and store in variable
 	while ((optIndex = getopt(argc, argv, ":j:")) != -1) {
 		switch (optIndex)
@@ -53,19 +65,40 @@ int main(int argc, char* const *argv) {
 		//REFERENCE -> https://linuxhint.com/using_mmap_function_linux/ Idea for using fstat
 		struct stat fileInfo;
 		fstat(fd, &fileInfo);
-		char *fileToRead = mmap(NULL, fileInfo.st_size, PROT_READ, MAP_SHARED, fd, 0);
+		char* fileToRead = mmap(NULL, fileInfo.st_size, PROT_READ, MAP_SHARED, fd, 0);
 		if (fileToRead == MAP_FAILED) {
 			perror("Map failed to open file in virtual memory\n"); //error checking
 			exit(1);
 		}
 		close(fd); //close opened file now that it is in memory
-		//Encode file now. 
-		encode_File(fileInfo.st_size, fileToRead);
+		//Encode file now.
+		char tempArr[8000];
+		encode_File(chunkSize, fileToRead, &fileOffset, tempArr);
+		
+		//fileOffset = fileInfo.st_size;
 		if (munmap(fileToRead, fileInfo.st_size) != 0) {
 			perror("Error unmapping file\n");
 			exit(1);
 		}
+		strcat(encode, tempArr);
 	}
-
+	
+	for (int i = 0; i < ARR_SIZE; ++i) {
+		if (encode[i]) {
+			if (encode[i + 3]) {
+				if (encode[i] == encode[i + 2]) {
+					printf("%c%c", encode[i], encode[i + 1] + encode[i + 3]);
+					i += 3;
+				}
+				else
+					printf("%c", encode[i]);
+			}
+			else
+				printf("%c", encode[i]);
+		}
+		else
+			break;
+	}
+	free(encode);
 	return 0;
 }
